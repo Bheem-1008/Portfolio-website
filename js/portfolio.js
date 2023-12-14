b61273017d4544f8feb5a1b466bee9c694997957
Bheemchauhan <chauhanbheem869@gmail.com>
2023-12-14 05:42:18 +0530

    edit portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,35 +6,37 @@ $(window).on("scroll", function () {
     }
 })
 
-//about animation
+// animation
 $(document).ready(() => {
     $(window).scroll(() => {
         let portfolio = $(document).scrollTop();
 
 
-        if ((portfolio > 450) && (portfolio < 700)) {
+        if ((portfolio > 400) && (portfolio < 500)) {
             $('#about-anim').addClass('animate__animated animate__zoomIn')
             $('#about2-anim').addClass('animate__animated animate__zoomIn')
         }
         // our services animation
-        else  if ((portfolio > 1020) && (portfolio < 1500)) {
+        else  if ((portfolio > 910) && (portfolio < 1000)) {
             $('#Serv-text').addClass('animate__animated animate__zoomIn');
             $('#card-one').addClass('animate__animated animate__fadeInLeft');
             $('#card-2').addClass('animate__animated animate__zoomIn');
             $('#card-three').addClass('animate__animated animate__fadeInRight');
         }
         //skill animation
-        else if( (portfolio > 1500) && (portfolio < 2000)) {
+        else if( (portfolio > 1300) && (portfolio < 1500)) {
             $('#skill-anim').addClass('animate__animated animate__zoomIn')
+            $('#skill1-anim').addClass('animate__animated animate__fadeInLeft')
+            $('#skill2-anim').addClass('animate__animated animate__fadeInRight')
         } 
         //contact animation
-        else if((portfolio > 2850) && (portfolio < 2900)) {
+        else if((portfolio > 1850) && (portfolio < 1900)) {
             $('#cont-anim').addClass('animate__animated animate__zoomIn')
             $('#cont2-anim').addClass('animate__animated animate__zoomIn')
             $('#cont3-anim').addClass('animate__animated animate__zoomIn')
         }
         //footer animation 
-        else if( (portfolio > 3100) && (portfolio < 3500)) {
+        else if( (portfolio > 2300) && (portfolio < 2520)) {
             $('#footer-anim').addClass('animate__animated animate__zoomIn')
         }
 
